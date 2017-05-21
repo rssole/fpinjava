ed0713c7160a73c7658bb1586668e4c501d5aad8
rastko <rssole@gmail.com>
2017-05-21 11:56:13 +0200

    Solved 2.9

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/test/java/com/fpinjava/functions/exercise02_09/FunctionExamplesTest.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/test/java/com/fpinjava/functions/exercise02_09/FunctionExamplesTest.java
@@ -1,18 +1,23 @@
 package com.fpinjava.functions.exercise02_09;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class FunctionExamplesTest {
 
   public static <A, B, C, D> String func(A a, B b, C c, D d) {
     return String.format("%s, %s, %s, %s", a, b, c, d);
   }
-  
+
   @Test
-  public void testF() {
+  public void testBookF() {
     assertEquals(func("A", "B", "C", "D"), FunctionExamples.f().apply("A").apply("B").apply("C").apply("D"));
+  }
+
+  @Test
+  public void testMyF() { // Because I try not to use stubbed exercise code at all :)
+    assertEquals(func("A", "B", "C", "D"), FunctionExamples2.f().apply("A").apply("B").apply("C").apply("D"));
   }
 
 }
