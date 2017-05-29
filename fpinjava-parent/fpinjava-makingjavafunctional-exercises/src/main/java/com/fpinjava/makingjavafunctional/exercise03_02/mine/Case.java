7098f3b3ce792054e9ad65a3c6220601f642a418
rastko <rssole@gmail.com>
2017-05-29 22:56:44 +0200

    Solved 3.2 :)

---
+++ fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_02/mine/Case.java
@@ -0,0 +1,38 @@
+package com.fpinjava.makingjavafunctional.exercise03_02.mine;
+
+
+import com.fpinjava.common.Tuple;
+import com.fpinjava.makingjavafunctional.exercise03_01.Result;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class Case<T> extends Tuple<Supplier<Boolean>, Supplier<Result<T>>> {
+
+    private Case(Supplier<Boolean> condition, Supplier<Result<T>> result) {
+        super(condition, result);
+    }
+
+    static <T> Case<T> mcase(Supplier<Boolean> condition,
+                             Supplier<Result<T>> value) {
+        return new Case<>(condition, value);
+    }
+    static <T> DefaultCase<T> mcase(Supplier<Result<T>> value) {
+        return new DefaultCase<>(value);
+    }
+
+    @SafeVarargs
+    static <T> Result<T> match(DefaultCase<T> defaultCase, Case<T>... matchers) {
+        final Optional<Case<T>> firstMatch = Arrays.stream(matchers)
+                .filter(m -> m._1.get())
+                .findFirst();
+        return firstMatch.isPresent() ? firstMatch.get()._2.get() : defaultCase._2.get();
+    }
+
+    static class DefaultCase<T> extends Case<T> {
+        DefaultCase(Supplier<Result<T>> value) {
+            super(() -> true, value);
+        }
+    }
+}
