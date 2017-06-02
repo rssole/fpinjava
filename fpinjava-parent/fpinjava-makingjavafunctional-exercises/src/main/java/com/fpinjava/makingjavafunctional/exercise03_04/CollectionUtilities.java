91bcd70eb3ef47f9b808254bf1f0f0b617a2d0be
rastko <rssole@gmail.com>
2017-06-02 20:11:03 +0200

    Solved 3.3 and 3.4

--- fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_04/CollectionUtilities.java
+++ fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_04/CollectionUtilities.java
@@ -8,32 +8,43 @@ import java.util.List;
 
 public class CollectionUtilities {
 
-  public static <T> List<T > list() {
-    return Collections.emptyList();
-  }
+    public static <T> List<T> list() {
+        return Collections.emptyList();
+    }
 
-  public static <T> List<T > list(T t) {
-    return Collections.singletonList(t);
-  }
+    public static <T> List<T> list(T t) {
+        return Collections.singletonList(t);
+    }
 
-  public static <T> List<T > list(List<T> ts) {
-    return Collections.unmodifiableList(new ArrayList<>(ts));
-  }
+    public static <T> List<T> list(List<T> ts) {
+        return Collections.unmodifiableList(new ArrayList<>(ts));
+    }
 
-  @SafeVarargs
-  public static <T> List<T > list(T... t) {
-    return Collections.unmodifiableList(Arrays.asList(Arrays.copyOf(t, t.length)));
-  }
+    @SafeVarargs
+    public static <T> List<T> list(T... t) {
+        return Collections.unmodifiableList(Arrays.asList(Arrays.copyOf(t, t.length)));
+    }
 
-  public static <T> T head(List<T> list) {
-    throw new RuntimeException("To be implemented");
-  }
+    public static <T> T head(List<T> list) {
+//        return list.isEmpty() ? null : list.get(0); // this was my initial attempt but in book - exception is thrown
+        if (list.isEmpty()) {
+            throw new IllegalStateException("head of empty list");
+        }
+        return list.get(0);
+    }
 
-  private static <T> List<T > copy(List<T> ts) {
-    throw new RuntimeException("To be implemented");
-  }
+    private static <T> List<T> copy(List<T> ts) {
+        return new ArrayList<>(ts);
+    }
 
-  public static <T> List<T> tail(List<T> list) {
-    throw new RuntimeException("To be implemented");
-  }
+    public static <T> List<T> tail(List<T> list) {
+        if (list.isEmpty()) {
+            throw new IllegalStateException("tail of empty list");
+        }
+        final List<T> copy = copy(list);
+        copy.remove(0);
+//        return list(copy); // this was my variant (to re-use available method but perhaps it is too much "copying" :)
+// in the book Collections::unmodifiableList is used as below
+        return Collections.unmodifiableList(copy);
+    }
 }
