836838508dde1f06a38a13b1b51cff5b03eddb12
rastko <rssole@gmail.com>
2017-06-02 23:39:21 +0200

    Solved 3.5 - well not exactly as it is solved in the book but... :)

--- fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_05/Fold.java
+++ fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_05/Fold.java
@@ -4,10 +4,17 @@ import com.fpinjava.common.Function;
 
 import java.util.List;
 
+import static com.fpinjava.makingjavafunctional.exercise03_04.CollectionUtilities.head;
+import static com.fpinjava.makingjavafunctional.exercise03_04.CollectionUtilities.tail;
+
 public class Fold {
 
-  public static Integer fold(List<Integer> is, Integer identity,
-                             Function<Integer, Function<Integer, Integer>> f) {
-    throw new RuntimeException("To be implemented");
-  }
+    public static Integer fold(List<Integer> is, Integer identity,
+                               Function<Integer, Function<Integer, Integer>> f) {
+        if (is.isEmpty()) {
+            return identity;
+        } else {
+            return f.apply(head(is)).apply(fold(tail(is), identity, f));
+        }
+    }
 }
