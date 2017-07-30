de3b1f70a6dab02bf9382645119d8ba29990da3b
rastko <rssole@gmail.com>
2017-07-30 13:15:54 +0200

    Solved 3.[11,12,13] :)

--- fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/test/java/com/fpinjava/makingjavafunctional/exercise03_14/RangeTest.java
+++ fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/test/java/com/fpinjava/makingjavafunctional/exercise03_14/RangeTest.java
@@ -1,8 +1,8 @@
 package com.fpinjava.makingjavafunctional.exercise03_14;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class RangeTest {
