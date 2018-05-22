cc95c480ed41e32eabb15e48ee10d67356f6d61d
rastko <rssole@gmail.com>
2018-05-22 22:43:56 +0200

    Solved 4.6 @ home

--- fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_06/ComposeAll.java
+++ fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_06/ComposeAll.java
@@ -1,13 +1,15 @@
 package com.fpinjava.recursion.exercise04_06;
 
+import com.fpinjava.common.Function;
+
 import java.util.List;
 
-import com.fpinjava.common.Function;
+import static com.fpinjava.common.CollectionUtilities.foldLeft;
 
 
 public class ComposeAll {
 
   static <T> Function<T, T> composeAll(List<Function<T, T>> list) {
-    throw new RuntimeException("To be implemented.");
+    return foldLeft(list, Function.identity(), f -> g -> f.compose(g));
   }
 }
