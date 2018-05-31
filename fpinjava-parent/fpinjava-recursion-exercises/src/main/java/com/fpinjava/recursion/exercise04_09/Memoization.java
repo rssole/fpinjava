147c045720c0e5f73e448e2a34b8a55259a2e7c4
Rastko Soskic <r.soskic.ext@unycom.com>
2018-05-31 11:26:28 +0200

    Solved 4.9

--- fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_09/Memoization.java
+++ fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_09/Memoization.java
@@ -1,15 +1,49 @@
 package com.fpinjava.recursion.exercise04_09;
 
+import com.fpinjava.common.TailCall;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
+import static com.fpinjava.common.CollectionUtilities.head;
+import static com.fpinjava.common.CollectionUtilities.tail;
+import static com.fpinjava.common.TailCall.ret;
+import static com.fpinjava.common.TailCall.sus;
 
 
 public class Memoization {
 
-  public static String fibo(int number) {
-    throw new RuntimeException("To be implemented.");
-  }
+    // My solution does not work for 0!
+    public static String fibo(int number) {
+        return makeString(fib_(number, BigInteger.ZERO, BigInteger.ONE, new ArrayList<>(Arrays.asList(BigInteger.ZERO, BigInteger.ONE))).eval(), ", ");
+    }
 
-  public static <T> String makeString(List<T> list, String separator) {
-    throw new RuntimeException("To be implemented.");
-  }
+    private static TailCall<List<BigInteger>> fib_(int x, BigInteger prev, BigInteger curr, List<BigInteger> acc) {
+        if (x == 0) {
+            return ret(Collections.singletonList(BigInteger.ZERO));
+        } else if (x == 1) {
+            return ret(acc);
+        } else {
+            return sus(() -> {
+                final BigInteger next = prev.add(curr);
+                acc.add(next);
+                return fib_(x - 1, curr, next, acc);
+            });
+        }
+    }
+
+    public static <T> String makeString(List<T> list, String separator) {
+        return _makeString(list, separator, new StringBuilder()).eval();
+    }
+
+    private static <T> TailCall<String> _makeString(List<T> list, String separator, StringBuilder acc) {
+        if (list.isEmpty()) {
+            return ret(acc.delete(acc.length() - 2, acc.length()).toString());
+        } else {
+            return sus(() -> _makeString(tail(list), separator, acc.append(head(list)).append(separator)));
+        }
+    }
 }
--- fpinjava-parent/fpinjava-recursion-exercises/src/test/java/com/fpinjava/recursion/exercise04_09/MemoizationTest.java
+++ fpinjava-parent/fpinjava-recursion-exercises/src/test/java/com/fpinjava/recursion/exercise04_09/MemoizationTest.java
@@ -1,13 +1,28 @@
 package com.fpinjava.recursion.exercise04_09;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class MemoizationTest {
 
-  @Test
-  public void testFibo() {
-    assertEquals("0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765", Memoization.fibo(20));
-  }
+    @Test
+    public void testFibo() {
+        assertEquals("0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765", Memoization.fibo(20));
+    }
+
+    @Test
+    public void testFiboZero() {
+        assertEquals("0", Memoization.fibo(0));
+    }
+
+    @Test
+    public void testFiboOne() {
+        assertEquals("0, 1", Memoization.fibo(1));
+    }
+
+    @Test
+    public void testFiboTwo() {
+        assertEquals("0, 1, 1", Memoization.fibo(2));
+    }
 }
